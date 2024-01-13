b8f11f121b15e27b239db7af3986ef1ac63c78b4
GraviousDante <toankhanh2003@gmail.com>
2024-01-13 20:55:21 +0700

    Android test

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:demo_session01/home_page.dart';
+import 'package:demo_session01/route.dart';
 import 'package:flutter/material.dart';
+import 'package:demo_session01/main_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,9 +18,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const homepage(),
-      debugShowCheckedModeBanner: false,
+      routes: routes,
+      home: MainPage(),
     );
   }
 }
-
---
+++ lib/route.dart
@@ -0,0 +1,8 @@
+import 'dart:js';
+
+import 'package:demo_session01/second_page.dart';
+import 'package:flutter/cupertino.dart';
+
+final Map <String, WidgetBuilder> routes = {
+  SecondPage.routeName : (context) => SecondPage()
+};
